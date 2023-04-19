6885aeca78c43c00905dcb4718180cb14059f5f4
Kodi <kodi.w13@gmail.com>
2023-04-19 17:46:35 -0600

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE usernames (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+
+
+);
+
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(20),
+  date DATE,
+  username_id INT,
+  roomname_id INT,
+  FOREIGN KEY (username_id) REFERENCES usernames(id),
+  FOREIGN KEY (roomname_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
